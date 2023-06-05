c3673ce4f24742e2cb06a1687764c24e73bbcd23
eotkd99 <eotkd99@gmail.com>
2023-06-05 20:17:20 +0900

    new layer

--- index.ts
+++ index.ts
@@ -5,6 +5,19 @@ import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 let map: google.maps.Map;
 let webGLOverlayView;
+const webToggle = localStorage.getItem('webToggle');
+const storedLatitude = localStorage.getItem('mapCenterLatitude');
+const storedLongitude = localStorage.getItem('mapCenterLongitude');
+const defaultLatitude = 0;
+const defaultLongitude = 0;
+const latitude = storedLatitude !== null ? parseFloat(storedLatitude) : defaultLatitude;
+const longitude = storedLongitude !== null ? parseFloat(storedLongitude) : defaultLongitude;
+let trafficLayer: google.maps.TransitLayer;
+let isTrafficLayerVisible = false;
+let transitLayer: google.maps.TransitLayer;
+let isTransitLayerVisible = false;
+let bicyclingLayer: google.maps.BicyclingLayer;
+let isBicyclingLayerVisible = false;
 
 const apiOptions = {
   apiKey: 'AIzaSyD6AUm1Y6hvKr3tSgSjzkpr9UK0wuCL5iI',
@@ -15,9 +28,9 @@ const mapOptions = {
   "tilt": 0,
   "heading": 0,
   "zoom": 18,
-  "center": { lat: 35.6594945, lng: 139.6999859 },
+  center: { lat: latitude, lng: longitude },
   "mapId": "5ad462e9fa6ea70e"
-  }
+}
 
 async function initMap() {
   const mapDiv = document.getElementById("map");
@@ -25,7 +38,6 @@ async function initMap() {
   await apiLoader.load();
   return new google.maps.Map(mapDiv, mapOptions);
 }
-
 
 function initWebGLOverlayView(map) {
   webGLOverlayView = new google.maps.WebGLOverlayView();
@@ -54,15 +66,18 @@ function initWebGLOverlayView(map) {
   }
 
   webGLOverlayView.onContextRestored = ({gl}) => {
-    // create the three.js renderer, using the
-    // maps's WebGL rendering context.
     renderer = new THREE.WebGLRenderer({
       canvas: gl.canvas,
       context: gl,
       ...gl.getContextAttributes(),
     });
     renderer.autoClear = false;
-
+    webGLOverlayView.onRemove = () => {
+      scene = null;
+      renderer = null;
+      camera = null;
+      loader = null;
+    };
     // wait to move the camera until the 3D model loads
     loader.manager.onLoad = () => {
       renderer.setAnimationLoop(() => {
@@ -85,7 +100,6 @@ function initWebGLOverlayView(map) {
   }
 
   webGLOverlayView.onDraw = ({gl, transformer}) => {
-    // update camera matrix to ensure the model is georeferenced correctly on the map
     const latLngAltitudeLiteral = {
       lat: mapOptions.center.lat,
       lng: mapOptions.center.lng,
@@ -98,24 +112,13 @@ function initWebGLOverlayView(map) {
     webGLOverlayView.requestRedraw();
     renderer.render(scene, camera);
 
-    // always reset the GL state
     renderer.resetState();
   }
-  webGLOverlayView.setMap(map);
 }
 
-(async () => {
-  if (condition) {
-    const map = await initMap();
-    initWebGLOverlayView(map);
-  } else {
-    // 다른 작업 수행
-  }
-})();
 
 function initAutocomplete() {
-  map = new google.maps.Map(document.getElementById("map") as HTMLElement,
-      {
+  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
         center: { lat: 35.1796, lng: 129.0756 },
         zoom: 10,
         mapTypeId: "roadmap",
@@ -124,35 +127,25 @@ function initAutocomplete() {
 
   const input = document.getElementById("pac-input") as HTMLInputElement;
   const searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  let markers: google.maps.Marker[] = [];
 
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
   map.addListener("bounds_changed", () => {
     searchBox.setBounds(map.getBounds() as google.maps.LatLngBounds);
   });
-
-  let markers: google.maps.Marker[] = [];
-
   searchBox.addListener("places_changed", () => {
     const places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-
-    // Clear out the old markers.
+    if (places.length == 0) return;
     markers.forEach((marker) => {
       marker.setMap(null);
     });
     markers = [];
-
     const bounds = new google.maps.LatLngBounds();
-
     places.forEach((place) => {
       if (!place.geometry || !place.geometry.location) {
         console.log("Returned place contains no geometry");
         return;
       }
-
       const icon = {
         url: place.icon as string,
         size: new google.maps.Size(71, 71),
@@ -160,7 +153,6 @@ function initAutocomplete() {
         anchor: new google.maps.Point(17, 34),
         scaledSize: new google.maps.Size(25, 25),
       };
-
       markers.push(
           new google.maps.Marker({
             map,
@@ -169,31 +161,80 @@ function initAutocomplete() {
             position: place.geometry.location,
           })
       );
-
-      if (place.geometry.viewport) {
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
+      if (place.geometry.viewport) bounds.union(place.geometry.viewport);
+      else bounds.extend(place.geometry.location);
     });
+
+    const center = bounds.getCenter();
+    const latitude = center.lat();
+    const longitude = center.lng();
+    localStorage.setItem('mapCenterLatitude', latitude);
+    localStorage.setItem('mapCenterLongitude', longitude);
+
     map.fitBounds(bounds);
   });
 
-  //트래픽 레이어 설정
-  const trafficLayer = new google.maps.TrafficLayer();
-  trafficLayer.setMap(map);
-  let toggleTrafficLayer = false;
+  if(webToggle==1){
+    (async () => {
+      const map = await initMap();
+      initWebGLOverlayView(map);
+      webGLOverlayView.setMap(map);
+    })();
+  }
+  function toggleWebGL() {
+    if (localStorage.getItem('webToggle')==1) {
+      // webToggle 값을 로컬 스토리지에 저장
+      localStorage.setItem('webToggle', 0);
+      location.reload();
+    } else {
+      // webToggle 값을 로컬 스토리지에 저장
+      localStorage.setItem('webToggle', 1);
+      location.reload();
+    }
+  }
+  window.toggleWebGL = toggleWebGL;
 
   function toggleTraffic() {
-    if (toggleTrafficLayer) {
+    if (!trafficLayer) {
+      trafficLayer = new google.maps.TrafficLayer();
+    }
+    if (isTrafficLayerVisible) {
       trafficLayer.setMap(null);
-      toggleTrafficLayer = false;
+      isTrafficLayerVisible = false;
     } else {
-      initWebGLOverlayView(map)
-      toggleTrafficLayer = true;
+      trafficLayer.setMap(map);
+      isTrafficLayerVisible = true;
     }
   }
   window.toggleTraffic = toggleTraffic;
+
+  function toggleTransitLayer() {
+    if (!transitLayer) {
+      transitLayer = new google.maps.TransitLayer();
+    }
+    if (isTransitLayerVisible) {
+      transitLayer.setMap(null);
+      isTransitLayerVisible = false;
+    } else {
+      transitLayer.setMap(map);
+      isTransitLayerVisible = true;
+    }
+  }
+  window.toggleTransitLayer = toggleTransitLayer;
+
+  function toggleBicycling() {
+    if (!bicyclingLayer) {
+      bicyclingLayer = new google.maps.BicyclingLayer();
+    }
+    if (isBicyclingLayerVisible) {
+      bicyclingLayer.setMap(null);
+      isBicyclingLayerVisible = false;
+    } else {
+      bicyclingLayer.setMap(map);
+      isBicyclingLayerVisible = true;
+    }
+  }
+  window.toggleBicycling = toggleBicycling;
 }
 
 declare global {
@@ -201,8 +242,9 @@ declare global {
     initAutocomplete: () => void;
     toggleTraffic: () => void;
     toggleWebGL: () => void;
+    toggleTransitLayer: () => void;
+    toggleBicycling: () => void;
   }
 }
-
 window.initAutocomplete = initAutocomplete;
 export {};
