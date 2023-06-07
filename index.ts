0f88a464e13585b49f09bfa2a5b691ee410965f2
eotkd99 <eotkd99@gmail.com>
2023-06-07 09:53:46 +0900

    split draw

--- index.ts
+++ index.ts
@@ -20,6 +20,11 @@ let transitLayer: google.maps.TransitLayer;
 let isTransitLayerVisible = false;
 let bicyclingLayer: google.maps.BicyclingLayer;
 let isBicyclingLayerVisible = false;
+let isPanoLayerVisible = false;
+let isSplitVisible=false;
+let isDrawVisible=false;
+let latestOverlay=null;
+let latestOverlayList=[];
 
 let animateOverlay ;
 let isAnimateOverlayVisible = false;
@@ -45,6 +50,9 @@ let props = {
   currentTime: 0,
   shadowEnabled: true,
 };
+let panorama;
+let mapLeft, mapRight;
+
 
 interface Data {
   vendor: number;
@@ -60,7 +68,7 @@ const apiOptions = {
 const mapOptions = {
   "tilt": 0,
   "heading": 0,
-  "zoom": 18,
+  "zoom": 17,
   center: { lat: latitude, lng: longitude },
   "mapId": "5ad462e9fa6ea70e"
 }
@@ -136,7 +144,7 @@ function initWebGLOverlayView(map) {
     const latLngAltitudeLiteral = {
       lat: mapOptions.center.lat,
       lng: mapOptions.center.lng,
-      altitude: 120
+      altitude: 100
     }
 
     const matrix = transformer.fromLatLngAltitude(latLngAltitudeLiteral);
@@ -157,6 +165,12 @@ function initAutocomplete() {
         mapId: '5ad462e9fa6ea70e',
       }
   );
+  const sv = new google.maps.StreetViewService();
+  panorama = new google.maps.StreetViewPanorama(
+      document.getElementById("pano")
+  );
+  sv.getPanorama({ location: map.getCenter(), radius: 50 }).then(processSVData);
+
   const input = document.getElementById("pac-input") as HTMLInputElement;
   const searchBox = new google.maps.places.SearchBox(input);
   let markers: google.maps.Marker[] = [];
@@ -296,6 +310,272 @@ function initAutocomplete() {
     }
   }
   window.Animate = Animate;
+
+  function Panorama(): void {
+    let mapElement: HTMLElement = document.getElementById("map")!;
+    let panoElement: HTMLElement = document.getElementById("pano")!;
+
+    if (isPanoLayerVisible) {
+      mapElement.style.width = "50%";
+      mapElement.style.height = "100%";
+      mapElement.style.float = "left";
+      mapElement.classList.add("hidden");
+      panoElement.style.width = "50%";
+      panoElement.style.height = "100%";
+      panoElement.style.float = "left";
+      panoElement.classList.add("hidden");
+      isPanoLayerVisible = false;
+    } else {
+      mapElement.style.width = "";
+      mapElement.style.height = "";
+      mapElement.style.float = "";
+      mapElement.classList.remove("hidden");
+      panoElement.style.width = "";
+      panoElement.style.height = "";
+      panoElement.style.float = "";
+      panoElement.classList.remove("hidden");
+      isPanoLayerVisible = true;
+    }
+  }
+  window.Panorama = Panorama;
+
+  function mapReset(): void {
+    location.reload();
+  }
+  window.mapReset = mapReset;
+
+  function toggleSplit() {
+    if (isSplitVisible) {
+      map.addListener("click", (event) => {
+        sv.getPanorama({ location: event.latLng, radius: 50 })
+            .then(processSVData)
+            .catch((e) =>
+                console.error("Street View data not found for this location.")
+            );
+      });
+      splitMapInit()
+      isSplitVisible = false;
+    } else {
+      map.removeListener("click", handleMapClick);
+      isSplitVisible = true;
+    }
+  }
+  window.toggleSplit = toggleSplit;
+
+  let drawingManager;
+  let marker = [];
+  let polygons = [];
+  let polylines = [];
+  let rectangles = [];
+  let circles = [];
+
+  drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.MARKER,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: [
+        google.maps.drawing.OverlayType.MARKER,
+        google.maps.drawing.OverlayType.CIRCLE,
+        google.maps.drawing.OverlayType.POLYGON,
+        google.maps.drawing.OverlayType.POLYLINE,
+        google.maps.drawing.OverlayType.RECTANGLE,
+      ],
+    },
+    markerOptions: {
+      icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+    },
+    circleOptions: {
+      fillColor: "#ffff00",
+      fillOpacity: 1,
+      strokeWeight: 5,
+      clickable: false,
+      editable: true,
+      zIndex: 1,
+    },
+  });
+
+
+  google.maps.event.addListener(drawingManager, 'overlaycomplete', function(event) {
+    console.log(drawingManager.drawingMode)
+    let overlay = event.overlay;
+    let overlayType = event.type;
+
+    switch (overlayType) {
+      case google.maps.drawing.OverlayType.MARKER:
+        marker.push(overlay);
+        break;
+      case google.maps.drawing.OverlayType.POLYGON:
+        polygons.push(overlay);
+        break;
+      case google.maps.drawing.OverlayType.POLYLINE:
+        polylines.push(overlay);
+        break;
+      case google.maps.drawing.OverlayType.RECTANGLE:
+        rectangles.push(overlay);
+        break;
+      case google.maps.drawing.OverlayType.CIRCLE:
+        circles.push(overlay);
+        overlay.setOptions({ editable: false });
+        break;
+      default:
+        break;
+    }
+    google.maps.event.addListener(overlay, 'click', function(event) {
+      latestOverlayList.push(overlay);
+      overlay.setOptions({ editable: true });
+    });
+  });
+  drawingManager.setMap(map);
+  function toggleDraw() {
+    if (isDrawVisible) {
+      drawingManager.setMap(map)
+      isDrawVisible = false;
+    } else {
+      drawingManager.setMap(null);
+      isDrawVisible = true;
+    }
+  }
+  window.toggleDraw = toggleDraw;
+}
+
+document.addEventListener('keydown', function(event) {
+  let element;
+  if (event.key === 'Delete' && latestOverlayList.length > 0) {
+    for (element of latestOverlayList) {
+      let overlayType = element.type;
+      switch (overlayType) {
+        case google.maps.drawing.OverlayType.MARKER:
+          marker.splice(marker.indexOf(element), 1);
+          break;
+        case google.maps.drawing.OverlayType.POLYGON:
+          polygons.splice(polygons.indexOf(element), 1);
+          break;
+        case google.maps.drawing.OverlayType.POLYLINE:
+          polylines.splice(polylines.indexOf(element), 1);
+          break;
+        case google.maps.drawing.OverlayType.RECTANGLE:
+          rectangles.splice(rectangles.indexOf(element), 1);
+          break;
+        case google.maps.drawing.OverlayType.CIRCLE:
+          circles.splice(circles.indexOf(element), 1);
+          break;
+        default:
+          break;
+      }
+      element.setMap(null);
+      element=null;
+    }
+  }
+});
+
+function processSVData({ data }) {
+  const location = data.location;
+  const marker = new google.maps.Marker({
+    position: location.latLng,
+    map,
+    title: location.description,
+  });
+
+  panorama.setPano(location.pano);
+  panorama.setPov({
+    heading: 270,
+    pitch: 0,
+  });
+  panorama.setVisible(true);
+  marker.addListener("click", () => {
+    const markerPanoID = location.pano;
+    panorama.setPano(markerPanoID);
+    panorama.setPov({
+      heading: 270,
+      pitch: 0,
+    });
+    panorama.setVisible(true);
+  });
+}
+
+function splitMapInit(){
+  const mapOptions = {
+    center: { lat: 44.5250489, lng: -110.83819 },
+    zoom: 18,
+    scaleControl: false,
+    streetViewControl: false,
+  };
+
+  // instantiate the map on the left with control positioning
+  mapLeft = new google.maps.Map(document.getElementById("map-left"), {
+    ...mapOptions,
+    mapTypeId: "satellite",
+    tilt: 0,
+    fullscreenControlOptions: {
+      position: google.maps.ControlPosition.LEFT_BOTTOM,
+    },
+    mapTypeControlOptions: {
+      position: google.maps.ControlPosition.LEFT_TOP,
+    },
+    zoomControlOptions: {
+      position: google.maps.ControlPosition.LEFT_BOTTOM,
+    },
+  });
+  // instantiate the map on the right with control positioning
+  mapRight = new google.maps.Map(document.getElementById("map-right"), {
+    ...mapOptions,
+    fullscreenControlOptions: {
+      position: google.maps.ControlPosition.RIGHT_BOTTOM,
+    },
+    mapTypeControlOptions: {
+      position: google.maps.ControlPosition.RIGHT_TOP,
+    },
+    zoomControlOptions: {
+      position: google.maps.ControlPosition.RIGHT_BOTTOM,
+    },
+  });
+
+  // helper function to keep maps in sync
+  function sync(...maps) {
+    let center, zoom;
+
+    function update(changedMap) {
+      maps.forEach((m) => {
+        if (m === changedMap) {
+          return;
+        }
+        m.setCenter(center);
+        m.setZoom(zoom);
+      });
+    }
+
+    maps.forEach((m) => {
+      m.addListener("bounds_changed", () => {
+        const changedCenter = m.getCenter();
+        const changedZoom = m.getZoom();
+
+        if (changedCenter !== center || changedZoom !== zoom) {
+          center = changedCenter;
+          zoom = changedZoom;
+          update(m);
+        }
+      });
+    });
+  }
+
+  sync(mapLeft, mapRight);
+
+  function handleContainerResize() {
+    const width = document.getElementById("container").offsetWidth;
+
+    document.getElementById("map-left").style.width = `${width}px`;
+    document.getElementById("map-right").style.width = `${width}px`;
+  }
+
+  // trigger to set map container size since using absolute
+  handleContainerResize();
+  // add event listener
+  window.addEventListener("resize", handleContainerResize);
+  //@ts-ignore
+  Split(["#left", "#right"], {
+    sizes: [50, 50],
+  });
 }
 
 window.initAutocomplete = initAutocomplete;
